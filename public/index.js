04bba99bd39e0bf97db94e04a4112b038346261a
Jacob Bashista <tgb.bashista@gmail.com>
2021-04-14 12:00:18 -0400

    Added Stage Picker and Global audio channel options

--- public/index.js
+++ public/index.js
@@ -1,19 +1,21 @@
 $(() => {
 
-     $('#urlmenu').change(() => {
-       let url = $('#urlmenu').val();
-       $('#mainurl').val(url);
-       console.log(url);
-     });
-     
-     fetch('/getstate')
+    $('#urlmenu').change(() => {
+        let url = $('#urlmenu').val();
+        $('#mainurl').val(url);
+        console.log(url);
+    });
+
+    // Get initial states
+    fetch('/getstate')
         .then(response => response.json())
         .then(data => {
             $('#mainurl').val(data.mainUrl);
             $('#muteAll').prop('checked', data.mute);
+            $('#stagemenu').val(data.stage);
         });
-     
-     $('#submitURL').click(() => {
+
+    $('#submitURL').click(() => {
         let url = $('#mainurl').val();
         console.log(url);
         fetch('/setkey', {
@@ -24,6 +26,20 @@ $(() => {
             body: JSON.stringify({
                 "key": "mainUrl",
                 "value": url
+            }),
+        });
+    });
+
+    $('#submitStage').click(() => {
+        let stage = $('#stagemenu').val();
+        fetch('/setkey', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "key": "stage",
+                "value": stage
             }),
         });
     });
@@ -41,6 +57,20 @@ $(() => {
             },
             body: JSON.stringify({
                 "key": "mute",
+                "value": checked
+            }),
+        });
+    });
+
+    $('#callBack').change(() => {
+        let checked = $('#callBack').prop('checked');
+        fetch('/setkey', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "key": "callback",
                 "value": checked
             }),
         });
