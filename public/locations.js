ee70b3f3f7cb3c03d16f6a04249b960e36150934
Jacob Bashista <tgb.bashista@gmail.com>
2021-07-01 17:15:15 -0400

    Basic Location Page

---
+++ public/locations.js
@@ -0,0 +1,20 @@
+$(() => {
+    setInterval(() => {
+        $.get("https://arjs-cors-proxy.herokuapp.com/https://teachers.jacobbashista.com/getlocations", function (data) {
+
+            $('#locationtable tbody').empty();
+
+            for (const [key, value] of Object.entries(data)) {
+
+                // using jquery insert a new row into the table
+
+                let timeago = Math.round(((Date.now() - value.timeStamp))/1000/60);
+
+                $('#locationtable tbody').append($('<tr>').append($('<td>').text(value.name)).append($('<td>').text(value.location)).append($('<td>').text(timeago + "min ago")));
+
+
+
+            }
+        });
+    }, 5000);
+});
