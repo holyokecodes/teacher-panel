2c42bebc70a429eb9597e3992c34ecedaa778ad2
Jacob Bashista <tgb.bashista@gmail.com>
2021-07-03 12:53:52 -0400

    Display Location Information Better

--- public/locations.js
+++ public/locations.js
@@ -1,19 +1,35 @@
 $(() => {
+
+    const locations = ['TrainingComplex', 'ControlRoomTeam1', 'ControlRoomTeam2', 'ControlRoomTeam3', 'ControlRoomTeam4'];
+
+
     setInterval(() => {
         $.get("/getlocations", function (data) {
 
-            $('#locationtable tbody').empty();
+            $('table tbody').empty();
 
             for (const [key, value] of Object.entries(data)) {
 
-                // using jquery insert a new row into the table
-
                 let timeago = Math.round(((Date.now() - value.timeStamp))/1000/60);
 
-                $('#locationtable tbody').append($('<tr>').append($('<td>').text(value.name)).append($('<td>').text(value.location)).append($('<td>').text(timeago + "min ago")));
+                let color = 'green';
+                if(timeago > 15) {
+                    color = 'yellow';
+                }
+                if(timeago > 30) {
+                    color = 'red';
+                }
+
+                let primary_location = value.location.split('_')[0];
+                let secondary_location = value.location.split('_')[1] || "";
+                let tertiary_location = value.location.split('_')[2] || "";
 
 
+                if(!locations.includes(primary_location)) {
+                    primary_location = "other";
+                }
 
+                $(`#${primary_location} tbody`).append($('<tr>').append($('<td>').text(value.name)).append($('<td>').text(secondary_location + ' ' + tertiary_location)).append($(`<td style="color: ${color};">`).text(timeago + "min ago")));
             }
         });
     }, 5000);
